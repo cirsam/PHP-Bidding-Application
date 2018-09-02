d0e6dd7147a15b8cf412ef17238b9697c30a9a3b
Samuel Antwi <samuelantwi@Samuels-MacBook-Pro.local>
2018-09-01 22:46:57 -0400

    rebuild application without unit test dues to time constraints

--- backend/app/Interfaces/ILoginSystem.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Bidding\Interfaces;
-
-interface ILoginSystem
-{
-	function signUp($params);
-	function signIn($params);
-	function signOut();
-}
-?>
--- backend/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once __DIR__ . '/vendor/autoload.php';
-	$loader->add('backend\\test\\', __DIR__);
-
-    use Bidding\Routes as Routes;
-    $route = new Routes;
-    $route->pages($_REQUEST);
-?>
